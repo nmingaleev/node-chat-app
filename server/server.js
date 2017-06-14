1fa8a59c2a98bff2d4435ff3d057a1454c00bd50
nmingaleev <nmingaleev@yandex.ru>
2017-06-14 10:37:55 +0700

    Great new user and alert others

--- server/server.js
+++ server/server.js
@@ -13,7 +13,20 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  console.log('New user connected'); 
+  console.log('New user connected');
+
+  socket.emit('newMessage', {
+    from: "Admin",
+    text: "Welcome to the chat app!",
+    createdAt: new Date().getTime()
+  })
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user connected',
+    createdAt: new Date().getTime()
+  })
+
 
   socket.on('createMessage', (message) => {
     io.emit('newMessage', {
@@ -21,7 +34,13 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     }); //emits an event to every single connection
-  })
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+  });
 
   socket.on('disconnect', () => {
     console.log('Disconnected');
